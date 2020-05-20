3755c3b34c93f27bf1cbd6b859d5d5df4c52cfa7
flo62134 <florentbrassart31@gmail.com>
2020-05-20 23:08:17 +0200

    Completed stage 2

--- Tic-Tac-Toe/task/tictactoe/tictactoe.py
+++ Tic-Tac-Toe/task/tictactoe/tictactoe.py
@@ -1,6 +1,25 @@
 # write your code here
-print('''
-X O X
-O X O
-X X O
-''')
+def print_border():
+    print('---------')
+
+
+def print_lines(grid_string: str):
+    chars = list(grid_string)
+    for x in range(0, 3):
+        print_line(chars, x)
+
+
+def print_line(grid_chars, line_number):
+    line = ''
+    line += '| '
+    for y in range(0, 3):
+        current_char = grid_chars[(3 * line_number) + y]
+        line += f"{current_char} "
+    line += '|'
+    print(line)
+
+
+grid = input()
+print_border()
+print_lines(grid)
+print_border()
